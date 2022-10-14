79ba1570d9abc41f8c03c0ef667adfe16884fc9a
Leandro <leandro.almeida@uvvnet.com.br>
2022-10-14 00:50:32 -0300

    identificação de cada botão

--- src/components/Button.js
+++ src/components/Button.js
@@ -33,11 +33,12 @@ export default props => {
     const stylesButton = [styles.button];
 
     if (props.double) stylesButton.push(styles.buttonDouble);
+    if (props.triple) stylesButton.push(styles.buttonTriple);
+    if (props.operation) stylesButton.push(styles.operationButton)
     
-
     return (
-        <TouchableHighlight onPress={props.onClick} >
-            <Text style={styles.button}>{props.label}</Text>
+        <TouchableHighlight onPress={() => props.onClick(props.label)} >
+            <Text style={stylesButton}>{props.label}</Text>
         </TouchableHighlight>
     )
 }
--- src/components/Display.js
+++ src/components/Display.js
@@ -2,13 +2,16 @@ import React from "react";
 import {
     StyleSheet,
     Text,
-    View
+    View,
+    ImageBackground,
+    Image,
+    Dimensions,
 } from 'react-native'
 
 const styles = StyleSheet.create({
     display: {
         flex: 1,
-        padding: 20,
+        padding: 0,
         justifyContent: 'center',
         backgroundColor: 'rgba(0,0,0,0.6)',
         alignItems: 'flex-end'
@@ -16,14 +19,31 @@ const styles = StyleSheet.create({
     displayValue: {
         fontSize: 60,
         color: '#fff'
+    },
+    imageBackground: {
+        flex: 1,
+        resizeMode: 'cover',
+        justifyContent: "center",
+        alignItems: "flex-end",
+        padding: 20,
+        width: Dimensions.get('window').width
+    },
+    txtName: {
+        color: '#fff',
+        paddingBottom: 160,
+        fontSize: 25,
+        marginRight: 45
     }
 })
 
 export default props => {
     return (
         <View style={styles.display}>
+            <ImageBackground source={require('../img/imagem.png')} style={styles.imageBackground}>
+                <Text style={styles.txtName}>Jacqueliny Calculator</Text>
             <Text style={styles.displayValue}
             numberOfLines={1}>{props.value}</Text>
+            </ImageBackground>
         </View>
     )
 }
